f198352bc9dff22e6f88233018cbd1b4df94cd0b
chehin <chehinjuan@gmail.com>
2021-11-29 10:56:26 -0300

    Maquetacion basica

---
+++ cau/view/MainNav/nav.php
@@ -0,0 +1,22 @@
+<nav class="side-menu">
+    <ul class="side-menu-list">
+	    <li>
+	        <a href="../Home/index.php">
+	            <i class="tag-color green"></i>
+	            <span class="lbl">Inicio</span>
+	        </a>
+	    </li>
+	    <li>
+	        <a href="../nuevo-ticket/index.php">
+	            <i class="tag-color grey-blue"></i>
+	            <span class="lbl">Nuevo Ticket</span>
+	        </a>
+	    </li>
+	    <li>
+	        <a href="../consulta-ticket/index.php">
+	            <i class="tag-color red"></i>
+	            <span class="lbl">Consultar Ticket</span>
+	        </a>
+	    </li>
+	</ul>
+</nav>
