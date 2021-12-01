4b8674a150d284e1930c06ef20f6a79bd84803fd
chehin <chehinjuan@gmail.com>
2021-12-01 07:47:55 -0300

    option de categoria + summernote para la descripcion

--- cau/view/NuevoTicket/nuevo-ticket.js
+++ cau/view/NuevoTicket/nuevo-ticket.js
@@ -1,5 +1,16 @@
-function init() {
+$(document).ready(function() {
+    $('#tick_descrip').summernote({
+        height: 150
+    });
 
+    $.post("../../controller/categoria.php?op=combo", function(data, status) {
+        console.log('data es : ', data);
+        $('#cat_id').html(data);
+    });
+
+});
+
+function init() {
     $("#ticket_form").on("submit", function(e) {
         guardaryeditar(e);
     });
