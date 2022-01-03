95f11aeb689ee8778120feca8c23333e39d19327
chehin <chehinjuan@gmail.com>
2022-01-03 14:06:06 -0300

    menu personalizado para rol

--- cau/view/MainNav/nav.php
+++ cau/view/MainNav/nav.php
@@ -1,22 +1,64 @@
-<nav class="side-menu">
-    <ul class="side-menu-list">
-	    <li>
-	        <a href="../Home/index.php">
-	            <i class="tag-color green"></i>
-	            <span class="lbl">Inicio</span>
-	        </a>
-	    </li>
-	    <li>
-	        <a href="../NuevoTicket/index.php">
-	            <i class="tag-color grey-blue"></i>
-	            <span class="lbl">Nuevo Ticket</span>
-	        </a>
-	    </li>
-	    <li>
-	        <a href="../consulta-ticket/index.php">
-	            <i class="tag-color red"></i>
-	            <span class="lbl">Consultar Ticket</span>
-	        </a>
-	    </li>
-	</ul>
-</nav>
+<?php
+    if ($_SESSION["rol_id"]==1){
+        ?>
+            <nav class="side-menu">
+                <ul class="side-menu-list">
+                    <li class="blue-dirty">
+                        <a href="..\Home\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Inicio</span>
+                        </a>
+                    </li>
+
+                    <li class="blue-dirty">
+                        <a href="..\NuevoTicket\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Nuevo Ticket</span>
+                        </a>
+                    </li>
+
+                    <li class="blue-dirty">
+                        <a href="..\ConsultarTicket\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Consultar Ticket</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        <?php
+    }else{
+        ?>
+            <nav class="side-menu">
+                <ul class="side-menu-list">
+                    <li class="blue-dirty">
+                        <a href="..\Home\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Inicio</span>
+                        </a>
+                    </li>
+
+                    <li class="blue-dirty">
+                        <a href="..\NuevoTicket\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Nuevo Ticket</span>
+                        </a>
+                    </li>
+
+                    <li class="blue-dirty">
+                        <a href="..\MntUsuario\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Mantenimiento Usuario</span>
+                        </a>
+                    </li>
+                    <li class="blue-dirty">
+                        <a href="..\ConsultarTicket\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Consultar Ticket</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        <?php
+    }
+?>
+
